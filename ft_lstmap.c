ae88a5d9e2c5d8e3e8e306735b0fb942663f8555
neoo-n <dana.vauthey@gmail.com>
2024-10-08 00:48:18 +0200

    night 08.10

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,16 +1,16 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvauthey <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:54:22 by dvauthey          #+#    #+#             */
-/*   Updated: 2024/10/07 15:02:10 by dvauthey         ###   ########.fr       */
+/*   Updated: 2024/10/07 23:39:15 by marvin           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
-#include <stdlib.h>
+#include <libft.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -21,8 +21,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	size_lst = ft_lstsize(lst);
 	i = 0;
 	new_lst = malloc(t_list);
+	if (new_lst == NULL)
+		return (NULL);
 	while (i < size_lst)
 	{
-		
+		new_lst->content = f(lst->content);
+		new_lst->next = lst->next;
 		i++;
-
+		
